860b762d2056dfa7d07595fa21213f93d7f70ee0
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-09-14 21:39:23 +0500

    code updated

--- TodoApp/index.php
+++ TodoApp/index.php
@@ -1,4 +1,4 @@
-
+<!-- shift alt f => code formatting -->
 <!-- database work -->
 
 <!-- 
@@ -47,25 +47,45 @@ if (!$con) {
     die("failed connection " . mysqli_connect_error());
 }
 
+
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $title = $_POST['title'];
-    $des = $_POST['des'];
-    $sql = "Insert into info(title, description)values('$title', '$des')";
-    $res = mysqli_query($con, $sql);
-
-    if ($res) {
-
-        // echo "  succuessfuly inserted <br>";
+    if (isset($_POST['editid'])) {
 
 
+        $id = $_POST['editid'];
+        $title = $_POST['titleEdit'];
+        $des = $_POST['desEdit'];
 
-        $insert = true;
+        $updatequery = "update info set Title='$title', description ='$des' where info.id='$id'";
+        $result = mysqli_query($con, $updatequery);
+        if ($result) {
+            $update = true;
+        } else {
+            echo "not updated";
+        }
     } else {
 
-        echo "  not inserted <br>";
+
+        $title = $_POST['title'];
+        $des = $_POST['des'];
+        $sql = "Insert into info(title, description)values('$title', '$des')";
+        $res = mysqli_query($con, $sql);
+
+        if ($res) {
+
+            // echo "  succuessfuly inserted <br>";
+
+
+
+            $insert = true;
+        } else {
+
+            echo "  not inserted <br>";
+        }
     }
 }
-
 ?>
 
 
@@ -77,16 +97,52 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>To Do App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-
     <!-- datatable.net cdn for css -->
     <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-   
-
 </head>
 
 <body>
 
+
+
+    <!-- Button trigger modal
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button> -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <form action="index.php" method="POST">
+                        <input type="hidden" name="editid" id="editid">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="titleEditt" name="titleEdit" aria-describedby="emailHelp">
+
+                        </div>
+                        <div class="form-floating">
+                            <textarea class="form-control" placeholder="Leave a comment here" name="desEdit" id="desEdit"></textarea>
+                            <label for="des">Description</label>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">To Do App</a>
@@ -120,6 +176,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
   </div>";
     }
+
+
+
+    if ($update) {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your task has been successfully updated.
+    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+  </div>";
+    }
     ?>
 
     <div class="container" style="margin-top: 50px ;">
@@ -141,7 +206,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     </div>
-    <div class="container" style="margin-top: 50px ;"  >
+    <div class="container" style="margin-top: 50px ;">
         <table class="table" id="myTable">
             <thead>
                 <tr>
@@ -167,9 +232,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         echo "<tr><td>" . $show['id'] . "</td>";
                         echo "<td>" . $show['title'] . "</td>";
                         echo "<td>" . $show['description'] . "</td>";
-                        echo
-                        "<td> 
-                    <button type='button' class='btn btn-dark'>Edit</button>
+                        echo "<td> 
+                    <button type='button' class='edit btn btn-dark'id=" . $show['id'] . ">Edit</button>
                     <button type='button' class='btn btn-dark'>Delete</button>
                     </td> 
                      </tr>";
@@ -205,5 +269,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $('#myTable').DataTable();
     });
 </script>
+
+
+
+<script>
+    edits = document.getElementsByClassName('edit');
+
+
+    Array.from(edits).forEach((element) => {
+
+        element.addEventListener("click", (e) => {
+
+            // console.log("edit sahi chal raha hai humara mubarak ho", e.target.parentNode.parentNode);
+            // yeh variable name hai or kuch nahi hai 
+            tr = e.target.parentNode.parentNode;
+            title = tr.getElementsByTagName("td")[1].innerText;
+            des = tr.getElementsByTagName("td")[2].innerText;
+            console.log(title, des)
+            titleEditt.value = title;
+            desEdit.value = des;
+            editid.value = e.target.id;
+            console.log("bhai yeh id hai laiba", e.target.id)
+            $('#editModal').modal('toggle');
+
+        })
+    })
+</script>
+
+
+
+
+
+
 
 </html>
