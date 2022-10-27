b30edb0dccd157917f65b77d6d308daaba38360e
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-10-27 17:52:44 +0500

    meny khan sahab ko salam kiya note pad ki file men

--- Lecture04Crudraw.php
+++ Lecture04Crudraw.php
@@ -1,184 +1,178 @@
 <?php
+
 $server = "localhost";
 $username = "root";
 $password = "";
+$database = "crudraw";
+
+
+// database connection
+$con = mysqli_connect($server, $username, $password, $database);
 
 
 
-//create connection
 
-$con = mysqli_connect($server, $username, $password, "engrac");
-// $sql = "create database engrac";
+if (!$con) {
+    echo die("connection failed" . mysqli_connect_error());
+} else {
+    echo "connection was successfull";
+}
+
+// **** CREATE DATABASE ****
+
+// $sql = "create database crudraw";
 // $resdata = mysqli_query($con, $sql);
 // if ($resdata) {
 
 //     echo "  succuessfuly created database <br>";
 // } else {
 
-//     echo "  notcreated database <br>";
+//     echo "  not created database <br>";
 // }
 
-if (!$con) {
-    die("failed connection " . mysqli_connect_error());
-} else {
-    echo "connection was successful<br>";
-}
 
 
 
+// **** CREATE TABLE ****
 
 
-// create table query
+// $sql = "create table info(
+//        id int auto_increment primary key not null,
+//        name varchar(250),
+//        email varchar(250),
+//        age int
 
-// $sql="Create Table engrac.info ( Id int auto_increment primary key not null,
-//                           Name varchar(250) not null,
-//                           Email varchar (250) not null,
-//                           Age varchar(250)  
-//                           )";
-//                           $restable=mysqli_query($con, $sql);
-//                           if ($restable) {
-//                               echo "Succuessfuly Table <br>";
-//                           } else {
-//                               echo "Not Table  <br>";
-//                           }
-
-
+//        )";
+// $restable = mysqli_query($con, $sql);
+// if ($restable) {
+//     echo "Succuessfuly Table <br>";
+// } else {
+//     echo "Not Table  <br>";
+// }
 
 
+// **** INSERT TABLE ****
 
-$name = "Engr.Misbah Iqbal  ";
-$email = "misbah@gmail.com";
+$name = "arshad shareef";
+$email = "m@gmail.com";
+$age = 123;
 
-$sql = "Insert into engrac.info(name, email)values('$name', '$email')";
+
+//  $sql="insert into info(name, email, age)values('$name', '$email', '$age')";
+
+
+//  $res=mysqli_query($con, $sql);
+
+//  if($res){
+//     echo"inserted successfully";
+//  }else{
+//     echo"not inserted";
+//  }
+
+
+
+$sql = "select *from info ";
+
 $res = mysqli_query($con, $sql);
 
-if ($res) {
 
-    echo "  succuessfuly inserted <br>";
-} else {
+// count rows
 
-    echo "  not inserted <br>";
+$numcounter = mysqli_num_rows($res);
+echo "<br>total rows" . $numcounter . "<br>";
+
+
+// **** FETCH SINGLE DATA ****
+
+
+
+// 1st row
+
+// $row=mysqli_fetch_assoc($res);
+// echo "<br>total rows".var_dump($row);
+
+// 2nd row
+
+// $row=mysqli_fetch_assoc($res);
+// echo "<br>total rows".var_dump($row);
+
+
+
+
+
+// **** FETCH COMPLETE DATA ****
+
+if ($numcounter > 0) {
+
+    while ($show = mysqli_fetch_assoc($res)) {
+
+        // echo "<br>total rows".var_dump($show);
+
+        echo "ID  =" . $show['id'] . "<br>";
+        echo "NAME  =" . $show['name'] . "<br>";
+        echo "EMAIL  =" . $show['email'] . "<br>";
+        echo "AGE  =" . $show['age'] . "<br>";
+    }
 }
-
-
-// ************insert query************
-
-
-
-
-
-
-// // ************select query************ 
-
-
-$sql1 = "select * from engrac.info ";
-// $res2 = mysqli_query($con, $sql1);
-// // //count rows
-
-
-
-// if ($num > 0) {
-//     while ($show = mysqli_fetch_assoc($res2)) {
-
-//         // echo var_dump($show) . "<br>";
-//         //datatype
-//         echo "Id " .    $show['Id'] . "<br>";
-
-//         echo "Name " .  $show['Name'] . "<br>";
-//         echo "Email " . $show['Email'] . "<br>";
-//         echo "Age " .   $show['Age'] . "<br>";
-//     }
-
-    // for fetch single data    
-    // $show=mysqli_fetch_assoc($res1);
-    // echo var_dump($show)."<br>";
-
-
-    // $show=mysqli_fetch_assoc($res1);
-    // echo var_dump($show)."<br>";
-
-    //     // $show=mysqli_fetch_assoc($res1);
-    //     // echo var_dump($show)."<br>";
-
-    //     // $show=mysqli_fetch_assoc($res1);
-    //     // echo var_dump($show)."<br>";
-
-
-
-    // }
-
-
-// }
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
 
 
 
+<!DOCTYPE html>
+<html lang="en">
 
- <!DOCTYPE html>
- <html lang="en">
-
- <head>
-     <meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- </head>
+</head>
 
- <body>
+<body>
 
 
     <table border="1">
+        <thead>
 
-         <thead>
-             <tr>
-                 <th>Id</th>
-               <th>Name</th>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
                 <th>Email</th>
+                <th>Age</th>
                 <th>Option</th>
-             </tr>
-         </thead>
-      <tbody>
+            </tr>
+        </thead>
 
-           <?php
+        <tbody>
 
-                            $res11 = mysqli_query($con, $sql1);
-                            $num = mysqli_num_rows($res11);
-                            if ($num > 0) {
+            <?php
 
-                                while ($show = mysqli_fetch_assoc($res11)) {
-
-                                    // echo var_dump($show) . "<br>";
-
-                                    echo "<tr><td>" . $show['Id'] . "</td>";
-                                    echo "<td>" . $show['Name'] . "</td>";
-                                    echo "<td>" . $show['Email'] . "</td>";
-                                    echo "<td> <button>Edit </button><button>Delete</button></td> 
-                                        </tr>";
-                                }
-                            }
-
-                            
-                ?>
+             // **** FETCH DATA IN TABULAR FORMAT ****
+            $sqlselect = "select *from info ";
+            $result = mysqli_query($con, $sqlselect);
+            while ($show = mysqli_fetch_assoc($result)) {
 
 
 
+                // echo "<br>total rows".var_dump($show);
+
+                echo "<tr> <td>" . $show['id'] . "</td>";
+                echo " <td>" . $show['name'] . "</td>";
+                echo " <td>" . $show['email'] . "</td>";
+                echo " <td>" . $show['age'] . "</td>";
+                echo "<td> <button>Edit </button>
+                <button>Delete</button></td></tr>";
+            }
+
+
+
+
+            ?>
         </tbody>
-
     </table>
-
 
 </body>
 
- </html>
+</html>
